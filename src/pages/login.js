7999f61849bd92e7bc9bd407cbc7ba4a60fcef15
Mauricio Munoz <dev.mauricio.munoz@gmail.com>
2019-07-31 00:13:50 -0500

    add signup form and authenticated routes

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import AppIcon from "../images/favicon.ico";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import themeFile from "../util/themeForm";
 
 //MUI
 import Grid from "@material-ui/core/Grid";
@@ -12,32 +13,7 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
-const styles = {
-  form: {
-    textAlign: "center"
-  },
-  image: {
-    margin: "20px auto 20px auto"
-  },
-  pageTitle: {
-    margin: "10px auto 10px auto"
-  },
-  textField: {
-    margin: "10px auto 10px auto"
-  },
-  button: {
-    marginTop: 20,
-    position: "relative"
-  },
-  customError: {
-    color: "red",
-    fontSize: "0.8rem",
-    marginTop: 10
-  },
-  progress: {
-    position: "absolute"
-  }
-};
+const styles = themeFile;
 
 class login extends Component {
   constructor() {
@@ -63,6 +39,7 @@ class login extends Component {
       .post("/login", userData)
       .then(response => {
         console.log(response.data);
+        localStorage.setItem("FBIdToken", `Bearer ${response.data.token}`);
         this.setState({
           loading: false
         });
@@ -137,7 +114,7 @@ class login extends Component {
             </Button>
             <br />
             <small>
-              no tienes una cuenta? registrate<Link to="/signup"> aqui</Link>
+              No tienes una cuenta? registrate<Link to="/signup"> aqui</Link>
             </small>
           </form>
         </Grid>
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,13 +1,187 @@
 import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import PropTypes from "prop-types";
+import AppIcon from "../images/favicon.ico";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import themeFile from "../util/themeForm";
+
+//MUI
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+const styles = themeFile;
 
 class signup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: "",
+      lastName: "",
+      profession: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      loading: false,
+      errors: {}
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      loading: true
+    });
+    const newUserData = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      profession: this.state.profession,
+      email: this.state.email,
+      password: this.state.password,
+      confirmPassword: this.state.confirmPassword
+    };
+    axios
+      .post("/signup", newUserData)
+      .then(response => {
+        console.log(response.data);
+        localStorage.setItem("FBIdToken", `Bearer ${response.data.token}`);
+        this.setState({
+          loading: false
+        });
+        this.props.history.push("/");
+      })
+      .catch(err => {
+        this.setState({
+          errors: err.response.data,
+          loading: false
+        });
+      });
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
   render() {
+    const { classes } = this.props;
+    const { errors, loading } = this.state;
     return (
-      <div>
-        <h1>signup page</h1>
-      </div>
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <img src={AppIcon} alt="Icon" className={classes.image} />
+          <Typography variant="h4" className={classes.pageTitle}>
+            Regístrate
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="firstName"
+              name="firstName"
+              type="text"
+              label="Nombre Completo"
+              helperText={errors.firstName}
+              error={errors.firstName ? true : false}
+              className={classes.textField}
+              value={this.state.firstName}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="lastName"
+              name="lastName"
+              type="text"
+              label="Apellidos"
+              helperText={errors.lastName}
+              error={errors.lastName ? true : false}
+              className={classes.textField}
+              value={this.state.lastName}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="profession"
+              name="profession"
+              type="text"
+              label="Profesión"
+              helperText={errors.profession}
+              error={errors.profession ? true : false}
+              className={classes.textField}
+              value={this.state.profession}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              className={classes.textField}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              className={classes.textField}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              label="confirmar Password"
+              helperText={errors.confirmPassword}
+              error={errors.confirmPassword ? true : false}
+              className={classes.textField}
+              value={this.state.confirmPassword}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            {errors.general && (
+              <Typography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typography>
+            )}
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              disabled={loading}
+            >
+              Registrarse
+              {loading && (
+                <CircularProgress size={30} className={classes.progress} />
+              )}
+            </Button>
+            <br />
+            <small>
+              Ya tienes una cuenta? ingresa<Link to="/login"> aqui</Link>
+            </small>
+          </form>
+        </Grid>
+        <Grid item sm />
+      </Grid>
     );
   }
 }
 
-export default signup;
+signup.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(signup);
